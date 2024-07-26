7032530a6fba60d64d52307ea5af14415c277c8a
mbm_1607 <muhammadkhan1607@gmail.com>
2024-07-26 12:44:20 +0500

    :art: Adjust formatting

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,9 +1,8 @@
 /** @type {import('prettier').Config} */
 const config = {
-	useTabs: true,
-	singleQuote: true,
-	jsxSingleQuote: true,
-	singleAttributePerLine: true,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  singleAttributePerLine: true,
 };
 
 module.exports = config;
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import {
-	EventEmitter,
-	NativeModulesProxy,
-	Subscription,
+  EventEmitter,
+  NativeModulesProxy,
+  Subscription,
 } from 'expo-modules-core';
 
 // Import the native module. On web, it will be resolved to UhfSerialReader.web.ts
@@ -13,21 +13,21 @@ import UhfSerialReaderModule from './UhfSerialReaderModule';
 export const PI = UhfSerialReaderModule.PI;
 
 export function hello(): string {
-	return UhfSerialReaderModule.hello();
+  return UhfSerialReaderModule.hello();
 }
 
 export async function setValueAsync(value: string) {
-	return await UhfSerialReaderModule.setValueAsync(value);
+  return await UhfSerialReaderModule.setValueAsync(value);
 }
 
 const emitter = new EventEmitter(
-	UhfSerialReaderModule ?? NativeModulesProxy.UhfSerialReader
+  UhfSerialReaderModule ?? NativeModulesProxy.UhfSerialReader
 );
 
 export function addChangeListener(
-	listener: (event: ChangeEventPayload) => void
+  listener: (event: ChangeEventPayload) => void
 ): Subscription {
-	return emitter.addListener<ChangeEventPayload>('onChange', listener);
+  return emitter.addListener<ChangeEventPayload>('onChange', listener);
 }
 
 export { ChangeEventPayload };
